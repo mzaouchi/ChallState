73a9c572b726613a3cc859e8f218b63334f5aa92
mzaouchi <mzaouchi@gmail.com>
2022-06-08 17:57:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      tasks: [
+        {title:'Pay internet',id:Math.random(),isdone:true},
+        {title:'Play weekend league',id:Math.random(),isdone :true},
+        {title:'Visit parents ',id:Math.random(),isdone :true},
+        {title:'Props ws',id:Math.random(),isdone :false},
+        {title:'Football game',id:Math.random(),isdone :false}
+      ],
+       assad: ""
+    }
+  }
+  handleAdd=()=> this.setState({tasks : [...this.state.tasks,{title:this.state.assad,id:Math.random(),isdone:false}]})
+  handleDelete=(a)=> this.setState({tasks : this.state.tasks.filter((el)=> el.id != a)})
+  handleEdit=(a)=> this.setState({tasks : this.state.tasks.map(el=> el.id == a ? {...el,isdone :  !el.isdone} : el )})
+  render(){
+    return(
+      <div>
+        <h1>Challenge State</h1>
+        {/* <h2>{this.state.tasks[2].title}</h2> */}
+        {
+          this.state.tasks.map((el,i)=> 
+          <div key={el.id}>
+            <h2 className={el.isdone && 'done'}>{el.title}</h2> 
+            <h2>{el.isdone ? 'TRUE' : 'FALSE'}</h2>
+            <button onClick={()=>this.handleDelete(el.id)}>Delete</button> 
+            <button onClick={()=>this.handleEdit(el.id)}>{el.isdone ? 'Not Done' : 'Done'}</button>
+          </div>    )
+        }
+        <br/>
+        <div>
+          <input type='text' onChange={(e)=>this.setState({assad : e.target.value})}/>
+          <button onClick={this.handleAdd}>Add</button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
